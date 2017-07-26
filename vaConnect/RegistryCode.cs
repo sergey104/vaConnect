b7adaa66f31b363dec1d5cab574ef858ce49ba80
sergey104 <sfilperz@gmail.com>
2017-07-26 10:44:46 +0300

    System registry records

---
+++ vaConnect/RegistryCode.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Windows.Forms;
+
+namespace vaConnect {
+    public class Register_it { 
+        public
+         void registry()
+           {
+             RegistryKey vaConnect1 = Registry.ClassesRoot.CreateSubKey("VaConnect");
+             vaConnect1.SetValue("", "URL:vaconnect Protocol");
+             vaConnect1.SetValue("URL Protocol", "");
+
+             RegistryKey defaultIcon = vaConnect1.CreateSubKey("DefaultIcon");
+             defaultIcon.SetValue("", Path.GetFileName(Application.ExecutablePath));
+
+             RegistryKey shell = vaConnect1.CreateSubKey("shell");
+             RegistryKey open = shell.CreateSubKey("open");
+             RegistryKey command = open.CreateSubKey("command");
+             command.SetValue("", Application.ExecutablePath + " %1");
+           }
+}
+        }
