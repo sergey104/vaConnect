9c8a0ddffa231f6f7b7ec5ea78ad19d026bcf5ce
Fil <Fil@DESKTOP-76PVTP6>
2017-08-01 14:51:28 +0300

    credential registration approach started

--- vaConnect/MainForm.cs
+++ vaConnect/MainForm.cs
@@ -17,6 +17,7 @@ using System.Diagnostics;
 using NativeWifi;
 using System.Linq;
 
+
 namespace vaConnect
 {
     /// <summary>
@@ -194,7 +195,18 @@ namespace vaConnect
                                             MessageText.Text = Ssid + " cannot set profile!";
                                             return;
                                         }
-                                      //  wlanIface.Connect(Wlan.WlanConnectionMode.Profile, Wlan.Dot11BssType.Any, wc.getxml());
+                                        try
+                                        {
+                                            wlanIface.Connect(Wlan.WlanConnectionMode.Profile, Wlan.Dot11BssType.Any,q.profileName);
+                                            
+                                            MessageText.AppendText( Ssid + " profile was connected!");
+                                        }
+                                        catch
+                                        {
+                                           
+                                            MessageText.AppendText(Ssid + " cannot connect!");
+                                            return;
+                                        }
                                     }
                                     break;
                                 }
@@ -270,7 +282,18 @@ namespace vaConnect
                                             MessageText.Text = Ssid + " cannot set profile!";
                                             return;
                                         }
-                                        //  wlanIface.Connect(Wlan.WlanConnectionMode.Profile, Wlan.Dot11BssType.Any, wc.getxml());
+                                        try
+                                        {
+                                            wlanIface.Connect(Wlan.WlanConnectionMode.Profile, Wlan.Dot11BssType.Any, q.profileName);
+
+                                            MessageText.AppendText(Ssid + " profile was connected!");
+                                        }
+                                        catch
+                                        {
+
+                                            MessageText.AppendText(Ssid + " cannot connect!");
+                                            return;
+                                        }
                                     }
                                     break;
                                 }
@@ -471,5 +494,54 @@ namespace vaConnect
                 System.Diagnostics.Process.Start(uristr);
           
         }
+        static public  void cred(String u, String p)
+        {
+            bool enteredPeapCred = false;
+            DateTime timePeapCredStarted = DateTime.Now.AddSeconds(10);
+
+            // wait for PEAP credentials window to appear (max. wait for 10 seconds)
+            while (!enteredPeapCred && timePeapCredStarted >= DateTime.Now)
+            {
+                IntPtr hwndLogon = Win32.FindWindow(null, "User Logon");
+
+                if (hwndLogon != IntPtr.Zero)
+                {
+                    // move User Logon window offscreen to prevent screen flicker in app
+                    Win32.MoveWindow(hwndLogon, -600, 0, 320, 480, true);
+
+                    // "Network Log On" label   
+                    IntPtr hwndCtrl1 = Win32.GetWindow(hwndLogon, Win32.GW_CHILD);
+                    // "Enter network info..." label
+                    IntPtr hwndCtrl2 = Win32.GetWindow(hwndCtrl1, Win32.GW_HWNDNEXT);
+                    // "User name:" label
+                    IntPtr hwndCtrl3 = Win32.GetWindow(hwndCtrl2, Win32.GW_HWNDNEXT);
+                    // username textbox
+                    IntPtr hwndCtrl4 = Win32.GetWindow(hwndCtrl3, Win32.GW_HWNDNEXT);
+                    // "Password:" label 
+                    IntPtr hwndCtrl5 = Win32.GetWindow(hwndCtrl4, Win32.GW_HWNDNEXT);
+                    // password textbox 
+                    IntPtr hwndCtrl6 = Win32.GetWindow(hwndCtrl5, Win32.GW_HWNDNEXT);
+                    // enter password into textbox
+                    StringBuilder sbPassword = new StringBuilder();
+                    sbPassword.Append(p);
+                    Win32.SetWindowText(hwndCtrl6, sbPassword);
+                    // "Domain:" label
+                    IntPtr hwndCtrl7 = Win32.GetWindow(hwndCtrl6, Win32.GW_HWNDNEXT);
+                    // domain textbox
+                    IntPtr hwndCtrl8 = Win32.GetWindow(hwndCtrl7, Win32.GW_HWNDNEXT);
+                    // "Save password" checkbox 
+                    IntPtr hwndCtrl9 = Win32.GetWindow(hwndCtrl8, Win32.GW_HWNDNEXT);
+                    // send BST_CHECKED message to set checkbox
+                   Win32.SendMessage(hwndCtrl9, Win32.BM_SETCHECK, Win32.BST_CHECKED, 0);
+
+                    // send WM_COMMAND with left softkey to submit user dialog
+                    IntPtr hwndMenu = Win32.SHFindMenuBar(hwndLogon);
+                    Win32.SendMessage(hwndLogon, Win32.WM_COMMAND, 0x2F87, hwndMenu.ToInt32());
+
+                    enteredPeapCred = true;
+                }
+            }
+        }
     }
+    
 }
